2b10805866fcbccae4e5e3d09c60338b89df5520
Bipul <sir.isac@gmail.com>
2015-02-19 03:18:01 +0530

    Update phpInstallation.sh

--- phpInstallation.sh
+++ phpInstallation.sh
@@ -23,11 +23,11 @@ clear
 
 #CHECKING PHP5 PACKAGES/DEPENDENCIES/INSTALLING
 	ee_echo "CheckING if you have PHP installed or not"
-	dpkg -s php5 &>> /dev/null && dpkg -s php5-fpm &>> /dev/null
+	dpkg -s php5 &>> /dev/null && dpkg -s php5-fpm &>> /dev/null && dpkg -s php5-mysql &>> /dev/null
 
 	if [ $? -ne 0 ]; then
 	        ee_fail "I need to install php5 with it's dependencies, please wait.."
-		 apt-get -y install php5 &>> /dev/null && apt-get -y install php5-fpm &>> /dev/null
+		 apt-get -y install php5 &>> /dev/null && apt-get -y install php5-fpm &>> /dev/null && apt-get -y install php5-mysql &>> /dev/null
 	else
 		ee_info "Dam !! you have PHP already installed"
 	fi
